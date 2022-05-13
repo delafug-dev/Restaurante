9ed5b19073bc03ff07dd654b10d448b1d2be8fa8
cosmologysk8 <a1999afg@gmail.com>
2022-05-13 18:44:12 +0200

    He terminado la clase Cuenta dentro de Camarero

--- src/main/java/bbdd/Productobd.java
+++ src/main/java/bbdd/Productobd.java
@@ -24,7 +24,7 @@ public class Productobd extends Configuracion{
 
                 //Recorremos los datos
                 while (rs.next()) {
-                    producto = new Producto(rs.getInt("id"), rs.getString("descripcion"),Tipoproducto.values()[rs.getInt("tipo_producto")],
+                    producto = new Producto(rs.getInt("id"), rs.getString("descripcion"), Tipoproducto.values()[rs.getInt("tipo_producto")],
                             rs.getDouble("pequenya"), rs.getDouble("media"), rs.getDouble("grande"));
                 }
 
@@ -50,8 +50,7 @@ public class Productobd extends Configuracion{
 
                 //Recorremos los datos
                 while (rs.next()) {
-                    Producto producto = new Producto(rs.getInt("id"), rs.getString("descripcion"),
-                            rs.getInt("tipo"), Tipoproducto.values()[rs.getInt("tipo_producto")],
+                    Producto producto = new Producto(rs.getInt("id"), rs.getString("descripcion"), Tipoproducto.values()[rs.getInt("tipo_producto")],
                             rs.getDouble("pequenya"), rs.getDouble("media"), rs.getDouble("grande"));
                 }
 
